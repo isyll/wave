0742798d9d05d3c870403fbc0fc5bbc513f85dc7
Ibrahima Sylla <isyll711@gmail.com>
2024-05-24 19:47:30 +0000

    fix: Widget binding initialization

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'firebase_options.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
