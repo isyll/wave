8b0dc0eb7e49c4f01dff806779f52f865d8ded49
Ibrahima Sylla <isyll711@gmail.com>
2024-05-24 19:28:56 +0000

    feat: define theme colors

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:waveapp/theme/colors.dart';
 
 class AppTheme {
-  static final light =
-      ThemeData(useMaterial3: true, brightness: Brightness.light);
+  static final light = ThemeData(
+      useMaterial3: true,
+      brightness: Brightness.light,
+      colorScheme: AppColors.lightColorScheme);
 }
---
+++ lib/theme/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const lightColorScheme = ColorScheme(
+      brightness: Brightness.light,
+      primary: Color(0xff4749d5),
+      onPrimary: Color(0xffffffff),
+      secondary: Color(0xffffffff),
+      onSecondary: Color(0x00000000),
+      tertiary: Color(0xff180399),
+      inversePrimary: Color(0xff7a7a7a),
+      error: Colors.red,
+      onError: Colors.white,
+      surface: Color(0xffffffff),
+      onSurface: Color(0xff180399));
+}
