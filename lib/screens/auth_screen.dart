fe1f61adfdd43514ccc4ba52331e53f6daf54ebd
Ibrahima Sylla <isyll711@gmail.com>
2024-05-24 18:32:50 +0000

    feat: create AuthScreen

---
+++ lib/screens/auth_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
