cd67d875f471500428c188fa32bf3dd9ef484124
Ibrahima Sylla <isyll711@gmail.com>
2024-05-24 18:39:46 +0000

    feat: create theme files

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class Theme {
+  static final light =
+      ThemeData(useMaterial3: true, brightness: Brightness.light);
+}
---
+++ lib/theme/text_theme.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class AppTextTheme {
+  static const light = TextTheme();
+}
