97aafcd869799f10cad50ae22c5e8a359080afe0
Ibrahima Sylla <isyll711@gmail.com>
2024-07-24 18:00:01 +0000

    move auth screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:waveapp/config/constants.dart';
-import 'package:waveapp/screens/auth_screen.dart';
+import 'package:waveapp/screens/auth/auth_screen.dart';
 import 'package:waveapp/screens/home/home_screen.dart';
 import 'package:waveapp/theme/theme.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
