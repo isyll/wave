07d8a235fcf6b0b153276565e32351e22a0ea9a8
Ibrahima Sylla <isyll711@gmail.com>
2024-07-24 21:50:35 +0000

    fix

--- lib/screens/auth/auth_screen.dart
+++ lib/screens/auth/auth_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:waveapp/screens/home/home_screen.dart';
 import 'package:waveapp/widgets/pin_code.dart';
 import 'package:loader_overlay/loader_overlay.dart';
 
