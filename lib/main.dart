a839416de2a51a22dc4344caec302ec72535412c
Ibrahima Sylla <isyll711@gmail.com>
2024-07-25 03:18:10 +0000

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -16,36 +16,15 @@ void main() async {
 
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
       .then((_) {
-    runApp(const App());
+    runApp( App());
   });
 }
 
-class App extends StatefulWidget {
-  const App({super.key});
+class App extends StatelessWidget {
+   App({super.key});
 
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
   final navigatorKey = GlobalKey<NavigatorState>();
 
-  @override
-  void initState() {
-    super.initState();
-    // initialization();
-  }
-
-//   void initialization() async {
-//     print('ready in 3...');
-//     await Future.delayed(const Duration(seconds: 1));
-//     print('ready in 2...');
-//     await Future.delayed(const Duration(seconds: 1));
-//     print('ready in 1...');
-//     await Future.delayed(const Duration(seconds: 1));
-//     print('go!');
-//     FlutterNativeSplash.remove();
-//   }
 
   @override
   Widget build(BuildContext context) {
